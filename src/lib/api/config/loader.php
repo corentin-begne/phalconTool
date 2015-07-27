8a7a08b376b5582dea5caba29632587d7e267a38
corentin-begne <corentin.begne@gmail.com>
2015-07-27 02:19:49 +0200

    lib dir

--- src/lib/api/config/loader.php
+++ src/lib/api/config/loader.php
@@ -1,9 +1,9 @@
 <?
 $includes = [
-    'Phalcon\lib\api\ApiController' => $config->application->rootDir.'vendor/v-cult/phalcon/src/lib/api/ApiController.php'
+    'Phalcon\lib\api\ApiController' => $config->application->libDir.'api/ApiController.php'
 ];
 if(!in_array('Phalcon\Tools\Rest', $loader->getClasses())){
-    $includes['Phalcon\Tools\Rest'] = $config->application->rootDir.'vendor/v-cult/phalcon/src/Tools/Rest.php';
+    $includes['Phalcon\Tools\Rest'] = $config->application->libDir.'../Tools/Rest.php';
 }
 $loader->registerClasses($includes)
 ->register();
