b60015ec1348c28653d7d0f01097b63cd2d4b762
corentin-begne <corentin.begne@gmail.com>
2015-09-26 00:50:53 +0200

    more

--- config/services.php
+++ config/services.php
@@ -6,15 +6,18 @@ $di = new CliDI();
 // Load the configuration file (if any)
 if(is_readable(APPLICATION_PATH . '/config/config.php')) {
     $config = include APPLICATION_PATH . '/config/config.php';
-    $di->set('config', $config);
-
+    $di->set('config', $config);   
     $di->set('db', function () use ($config) {
-        return new Mysql(array(
-            'host' => $config[ENV]->database->host,
-            'username' => $config[ENV]->database->username,
-            'password' => $config[ENV]->database->password,
-            'dbname' => $config[ENV]->database->dbname,
-            "charset" => $config[ENV]->database->charset
-        ));
-    });
+        try{
+            return new Mysql([                
+                'host' => $config[ENV]->database->host,
+                'username' => $config[ENV]->database->username,
+                'password' => $config[ENV]->database->password,
+                'dbname' => $config[ENV]->database->dbname,
+                "charset" => $config[ENV]->database->charset
+            ]);
+        } catch(\PDOException $e) {
+            Phalcon\Tools\Cli::error($e->getMessage());
+        }
+    });    
 }
