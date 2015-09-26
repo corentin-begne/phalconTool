40d9a0575876a18793f11ab1e7b2cb53c13c9b21
corentin-begne <corentin.begne@gmail.com>
2015-09-26 14:56:32 +0200

    more

--- src/lib/scrud/config/loader.php
+++ src/lib/scrud/config/loader.php
@@ -1,6 +1,6 @@
 <?
 $loader->registerClasses([
-    'ScrudController' => $config->application->libDir.'api/ApiController.php',
+    'ScrudController' => $config->application->libDir.'scrud/ScrudController.php',
     'Rest' => $config->application->libDir.'../Tools/Rest.php',
     'Phalcon\Builder\Form' => $config->application->libDir.'../Builder/Form.php'
 ], true);
