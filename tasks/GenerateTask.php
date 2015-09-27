7d2408044ff9f68ffad1d40ef6ad160111ba8442
corentin-begne <corentin.begne@gmail.com>
2015-09-27 03:29:57 +0200

    more

--- tasks/GenerateTask.php
+++ tasks/GenerateTask.php
@@ -27,7 +27,10 @@ class GenerateTask extends \Phalcon\CLI\Task
             Cli::warning('Don\'t forget to modify your app config', true);
             echo $appPath."/config/config.php\n";
             // public folder
-            exec('cp -r '.TEMPLATE_PATH.'/project/public/app/* '.HOME_PATH.'/public/'.$appName);
+            exec('cp -r '.TEMPLATE_PATH.'/project/public/app/css '.HOME_PATH.'/public/'.$appName.'/');
+            exec('cp -r '.TEMPLATE_PATH.'/project/public/app/js '.HOME_PATH.'/public/'.$appName.'/');
+            exec('cp -r '.TEMPLATE_PATH.'/project/public/app/img '.HOME_PATH.'/public/'.$appName.'/');
+            exec('ln -s '.TEMPLATE_PATH.'/../src/lib/ '.HOME_PATH.'/public/'.$appName.'/lib');
             Cli::success('app '.$appName.' successfully created');
         } else {
             Cli::error('app folder '.$appName.' already exists');
