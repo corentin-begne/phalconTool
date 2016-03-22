22669e91f5afcebe973b2518b582d6caaf2c473e
Corentin Bègne <corentin.begne@gmail.com>
2016-03-22 11:39:51 +0100

    more

--- src/Websocket/User.php
+++ src/Websocket/User.php
@@ -5,7 +5,9 @@ class User {
 
     public $socket;
     public $id;
-    public $headers = array();
+    public $headers = [];
+    public $room;
+    public $data;
     public $handshake = false;
 
     public $handlingPartialPacket = false;
@@ -19,5 +21,11 @@ class User {
     function __construct($id, $socket) {
         $this->id = $id;
         $this->socket = $socket;
+    }
+
+    public function parseData(){
+        list($room, $data) = explode('?', $this->requestedResource);
+        $this->room = trim($room, '/');
+        parse_str($data, $this->data);
     }
 }
