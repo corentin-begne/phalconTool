68bd13a6592f80820b675d6dfe78f1091f365aed
Corentin Bègne <corentin.begne@gmail.com>
2016-04-21 12:24:35 +0200

    fix ext default main js

--- templates/project/public/app/js/index/main.js
+++ templates/project/public/app/js/index/main.js
@@ -1,7 +1,7 @@
 /*global IndexManager, require */
 var indexManager;
 (function(){
-    require(["/bower_components/jquery/dist/jquery.min"], ready);
+    require(["/bower_components/jquery/dist/jquery.min.js"], ready);
 
     function ready(){
         /** on document ready */
@@ -13,7 +13,7 @@ var indexManager;
          * @description initialize index
          */
          function init(){
-            require(["/bower_components/cb-helpers/js.min"], loaded);
+            require(["/bower_components/cb-helpers/js.min.js"], loaded);
 
             function loaded(){
                 new JsHelper();
