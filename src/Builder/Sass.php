09b551f91e045ec5be92144800e6872580889676
Corentin Bègne <corentin.begne@gmail.com>
2021-03-14 20:24:10 +0100

    get sass config prod

--- src/Builder/Sass.php
+++ src/Builder/Sass.php
@@ -14,11 +14,33 @@ class Sass extends \Phalcon\DI\Injectable
         } else {
             $files = self::globRecursive($basePath."main.scss");
         }
+        if(ENV === 'prod'){
+            $config = $this->config->application->publicDir.'css/include/defines/config.scss';
+            if(file_exists($config)){
+                $content = file_get_contents($config);
+                $cdn = '';
+                if(isset($this->config[ENV]->cdns)){
+                    if(defined('CDN')){
+                        $cdn = $this->config[ENV]->cdns[CDN];
+                    } else {
+                        foreach($this->config[ENV]->cdns as $url){
+                            $cdn = $url;
+                            break;
+                        }
+                    }
+                }
+                $cdn .= '/'.APP.'/';
+                file_put_contents($config, '$basePath:"'.$cdn.'";$version:'.$this->config->version.';');
+            }            
+        }
         foreach($files as $file){
             $targetFile = str_replace('.scss', '.css', $file);
             // compile and compress css
             exec('sass --style=compressed '.$file.' '.$targetFile);
             echo $targetFile."\n";
+        }
+        if(ENV === 'prod' && isset($content)){
+            file_put_contents($config, $content);
         }
     }
 
