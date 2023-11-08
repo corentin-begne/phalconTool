59093a579cd3b3b448fe0bd5a6451c6087e28427
Redmine Admin <corentin.begne@sclepios-ia.com>
2023-11-08 01:57:53 +0100

    fix release task

--- tasks/ReleaseTask.php
+++ tasks/ReleaseTask.php
@@ -13,20 +13,21 @@ class ReleaseTask extends Task
     */    
     public function mainAction():void{
         // need to change the env in the init file
-        $file = str_replace("'dev'", "'ENV'", file_get_contents($this->config->application->rootDir.'public/init.php'));
+        $file = str_replace("'dev'", "'".ENV."'", file_get_contents($this->config->application->rootDir.'public/init.php'));
         file_put_contents($this->config->application->rootDir.'public/init.php', $file);
         if(ENV !== 'dev'){
-            exec($this->config->application->rootDir.'phalcon generate:build --module --env='.ENV);
+            exec($this->config->application->rootDir.'phalcon generate:build');
             exec($this->config->application->rootDir.'phalcon generate:sass --env='.ENV);
         }
         /** upload files */
         $exclude = '" --exclude="';
-        $cmd = 'rsync -zvr --links '.(isset($this->config[ENV]['mep']['args']) ? ' '.$this->config[ENV]['mep']['args'].' ' : '').(isset($this->config[ENV]['mep']['key']) ? '-e "ssh -i '.$this->config[ENV]['mep']['key'].'" ' : '').(isset($this->config[ENV]['mep']['excludes']) ? '--exclude="'.implode($exclude, $this->config[ENV]['mep']['excludes']->toArray()).'" ' : '').$this->config->application->rootDir.'* '.$this->config[ENV]['mep']['ssh'];
+        $cmd = 'rsync -zvr --links '.(isset($this->config[ENV]['release']['args']) ? ' '.$this->config[ENV]['release']['args'].' ' : '').(isset($this->config[ENV]['release']['key']) ? '-e "ssh -i '.$this->config[ENV]['release']['key'].'" ' : '').(isset($this->config[ENV]['release']['excludes']) ? '--exclude="'.implode($exclude, $this->config[ENV]['release']['excludes']->toArray()).'" ' : '').$this->config->application->rootDir.'* '.$this->config[ENV]['release']['ssh'];
+        echo $cmd."\n";
         exec($cmd);
         if(ENV !== 'dev'){
             exec($this->config->application->rootDir.'phalcon generate:sass --env=dev');                  
         }
-        $file = str_replace("'ENV'", "'dev'", $file);  
+        $file = str_replace("'".ENV."'", "'dev'", $file);  
         file_put_contents($this->config->application->rootDir.'public/init.php', $file);
     }
 }
