13dfefa2a64c273870d1ac51a1a1805380d46641
Corentin Bègne <corentin.begne@sclepios-ia.com>
2025-10-17 16:29:49 +0200

    post hook release

--- tasks/ReleaseTask.php
+++ tasks/ReleaseTask.php
@@ -24,6 +24,11 @@ class ReleaseTask extends Task
         $cmd = 'rsync -zvr --links '.(isset($this->config[ENV]['release']['args']) ? ' '.$this->config[ENV]['release']['args'].' ' : '').(isset($this->config[ENV]['release']['key']) ? '-e "ssh -i '.$this->config[ENV]['release']['key'].'" ' : '').(isset($this->config[ENV]['release']['excludes']) ? '--exclude="'.implode($exclude, $this->config[ENV]['release']['excludes']->toArray()).'" ' : '').realpath($this->config->application->rootDir).'/ '.$this->config[ENV]['release']['ssh'];
         echo $cmd."\n";
         exec($cmd);
+        if(isset($this->config[ENV]['release']['post_hook'])){
+            $cmd = 'rsync -zvr '.(isset($this->config[ENV]['release']['post_hook']['args']) ? ' '.$this->config[ENV]['release']['post_hook']['args'].' ' : '').(isset($this->config[ENV]['release']['key']) ? '-e "ssh -i '.$this->config[ENV]['release']['key'].'" ' : '').(isset($this->config[ENV]['release']['post_hook']['excludes']) ? '--exclude="'.implode($exclude, $this->config[ENV]['release']['post_hook']['excludes']->toArray()).'" ' : '').$this->config[ENV]['release']['post_hook']['path'].' '.$this->config[ENV]['release']['ssh'].'/'.$this->config[ENV]['release']['post_hook']['path'];
+            echo 'post hook command : '.$cmd."\n";
+            exec($cmd);
+        }
         if(defined('BUILD')){
             exec($this->config->application->rootDir.'phalcon generate:sass --env=dev');                  
         }
